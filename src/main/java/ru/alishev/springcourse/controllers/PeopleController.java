b4ba1a60aa5f312bdfb3d8068c1912e1dbc933e8
kosti <kostia.xn@gmail.com>
2023-05-10 13:28:20 +0300

    CRUD, REST, Паттерн DAO (Data Access Object).

---
+++ src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -0,0 +1,36 @@
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.alishev.springcourse.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+
+    private final PersonDAO personDAO;
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+
+    @GetMapping()
+    public String index(Model model) {
+        // Получим всех людей из DAO и передадим на отображениев представление
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // Получим одного человека по его id из DAO и передадим на отображдение  в представление
+        model.addAttribute("person",personDAO.show(id));
+        return "people/show";
+    }
+}
