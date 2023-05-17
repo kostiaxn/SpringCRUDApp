6f7b3b24c97766cbf634a4976867d2396f66dcc2
kosti <kostia.xn@gmail.com>
2023-05-17 09:47:30 +0300

    Spring Validator

--- src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -8,7 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.alishev.springcourse.dao.PersonDAO;
 import ru.alishev.springcourse.models.Person;
-
+import ru.alishev.springcourse.util.PersonValidator;
 
 
 @Controller
@@ -16,10 +16,12 @@ import ru.alishev.springcourse.models.Person;
 public class PeopleController {
 
     private final PersonDAO personDAO;
+    private final PersonValidator personValidator;
 
     @Autowired
-    public PeopleController(PersonDAO personDAO) {
+    public PeopleController(PersonDAO personDAO, PersonValidator personValidator) {
         this.personDAO = personDAO;
+        this.personValidator = personValidator;
     }
 
     @GetMapping()
@@ -42,6 +44,8 @@ public class PeopleController {
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult) {
+        personValidator.validate(person,bindingResult);
+
         if (bindingResult.hasErrors())
             return "people/new";
 
@@ -58,6 +62,7 @@ public class PeopleController {
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
+        personValidator.validate(person,bindingResult);
         if (bindingResult.hasErrors())
             return "people/edit";
 
